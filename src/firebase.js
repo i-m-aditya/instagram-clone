78d11d97275eb851d16b1c79a7dcec45ec9b2f4e
Aditya Pandey <aditya.adityapandey02@gmail.com>
2020-08-27 16:15:57 +0530

    code reformatting

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase'
 
-const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyALFHsgY7FF3-iy3MKs512Z-b3NRw1LKAU",
-    authDomain: "instagram-ditto.firebaseapp.com",
-    databaseURL: "https://instagram-ditto.firebaseio.com",
-    projectId: "instagram-ditto",
-    storageBucket: "instagram-ditto.appspot.com",
-    messagingSenderId: "397263649715",
-    appId: "1:397263649715:web:3891943fc2a14ab1e569c6",
-    measurementId: "G-NCK6EY0NCT"
-});
+// const firebaseApp = firebase.initializeApp({
+//     apiKey: "AIzaSyALFHsgY7FF3-iy3MKs512Z-b3NRw1LKAU",
+//     authDomain: "instagram-ditto.firebaseapp.com",
+//     databaseURL: "https://instagram-ditto.firebaseio.com",
+//     projectId: "instagram-ditto",
+//     storageBucket: "instagram-ditto.appspot.com",
+//     messagingSenderId: "397263649715",
+//     appId: "1:397263649715:web:3891943fc2a14ab1e569c6",
+//     measurementId: "G-NCK6EY0NCT"
+// });
 
 const db = firebase.firestore();
 const auth = firebase.auth();
